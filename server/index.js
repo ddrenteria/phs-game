22f2e2753e74fda97b5a0fa85968bd0dd7c5c41d
Dago Martin de Renteria <drenteria@dc.uba.ar>
2020-10-09 01:16:37 -0300

    first commit half way through

---
+++ server/index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const app = express();
+const mysql = require('mysql')
+const request = require('request');
+const cron = require('node-cron');
+const cors = require('cors');
+
+const randomDataGenerator = require('./randomDataGenerator');
+const bodyParser = require("body-parser");
+const { on } = require("nodemon");
+let lastUpdate = Date.now();
+
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'phs-gameDB',
+});
+
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended : true }))
+
+// Schedule tasks to be run on the server.
+cron.schedule('5 * * * * *', function() {
+    console.log("cron job runned")
+    randomDataGenerator.randomDataGenerator();
+});
+
+
+
+app.get("/api/get", (req, res) => {           // aca hacer esta funcion con select top 10
+     const sqlGetTopTen = "SELECT * FROM Players LIMIT 10"; 
+     const sqlGetTopTen2 =  `
+     SELECT p.nickname, p.profile_image, s.value 
+         FROM Players as p 
+         INNER JOIN player_has_score as ps ON ps.player_id = p.id 
+         INNER JOIN Scores as s ON s.id = ps.score_id 
+     ORDER BY s.value DESC LIMIT 0, 10;    
+     `;
+    db.query(sqlGetTopTen2, (err, result) => {
+        res.send(result);
+    });
+});
+
+
+app.get("/api/generate", (req, res) => {
+    lastUpdate = randomDataGenerator.randomDataGenerator();
+    res.send("{"+lastUpdate+"}");
+});
+
+app.listen(3001, () => {
+    console.log("running on port 3001");
+});
---
+++ server/randomDataGenerator.js
@@ -0,0 +1,118 @@
+const axios = require('axios');
+const mysql = require('mysql');
+const bodyParser = require("body-parser");
+
+//unsecure
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'phs-gameDB',
+});
+
+function getNumberBetween(min, max) {
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+async function getRandomNickname(){
+    return await axios.get('https://randomuser.me/api')
+    .then(response => {
+        //console.log(response.data.results[0].login.username);
+        return response.data.results[0].login.username;
+    })
+    .catch(error => {
+        console.log(error);
+    });
+}
+
+async function getRandomProfileImage(){
+    return await axios.get('https://randomuser.me/api')
+    .then(response => {
+       // console.log(response.data.results[0].picture.thumbnail);
+        return response.data.results[0].picture.thumbnail;
+    })
+    .catch(error => {
+        console.log(error);
+    });
+}
+
+async function insertInScores(value, timestamp) {
+    const sqlInsertScore = 'INSERT INTO Scores(value, timestamp) VALUES ( ?, ?);';
+    return new Promise(function (resolve, reject) {
+        db.query(sqlInsertScore,[value, timestamp], function (error, results, fields) {
+        if (error) { return console.log(error); }
+        // error will be an Error if one occurred during the query
+        // results will contain the results of the query
+        // fields will contain information about the returned results fields (if any)
+        resolve(results.insertId);
+    });}
+    )
+}
+
+async function insertInPlayers(nickname, profileImage) {
+    const sqlInsert = 'INSERT INTO Players(nickname, profile_image) VALUES ( ? , ?);';
+    return new Promise(function (resolve, reject) {
+        db.query(sqlInsert, [nickname, profileImage], function (error, results, fields) {
+        if (error) { return console.log(error); }
+        // error will be an Error if one occurred during the query
+        // results will contain the results of the query
+        // fields will contain information about the returned results fields (if any)
+        resolve(results.insertId);
+    });}
+    )
+}
+
+
+async function generateScoreForPlayer() {
+    //mySql
+    const nickname = await getRandomNickname();
+    console.log(nickname);
+    const profileImage = await getRandomProfileImage();
+    const timestamp = Date.now();
+    const value = getNumberBetween(1, 100);
+    
+    const scoreId = await insertInScores(value, timestamp);
+    const playerId = await insertInPlayers(nickname, profileImage);
+    console.log(playerId);
+    console.log(scoreId);
+    
+    const sqlInsertPxS = 'INSERT INTO player_has_score(player_id,score_id) VALUES (?,?);'
+    db.query(sqlInsertPxS, [playerId, scoreId], function (error, results, fields) {
+    });
+    
+    
+}
+
+function randomDataGenerator() {
+    const amountOfPlayers = getNumberBetween(0,10);
+    for (i = 0; i < amountOfPlayers; i++) {
+        generateScoreForPlayer();
+    }
+    return Date.now();
+}
+
+
+module.exports = {randomDataGenerator};
+
+    // CREATE TABLE scores
+    //     ( id INT(11) NOT NULL AUTO_INCREMENT,
+    //     value INT(11) NOT NULL,
+    //     timestamp DATE,
+    //     CONSTRAINT scores_pk PRIMARY KEY (id)
+    //     );
+    
+    // CREATE TABLE players
+    // ( id INT(11) NOT NULL AUTO_INCREMENT,
+    //   nickname VARCHAR(8000),
+    //   profile_image VARCHAR((8000)),
+    //   CONSTRAINT players_pk PRIMARY KEY (id)
+    // );
+
+    // CREATE TABLE phs.gameDB.player_has_score
+    // ( player_id INT(11) NOT NULL,
+    //   score_id INT(11) NOT NULL,
+    //   foreign key(player_id) references Players,
+    //   foreign key(score_id) references Scores
+    // )
